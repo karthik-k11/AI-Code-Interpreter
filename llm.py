829ddf4e0eb4ef99994e01c1216550d63ee8fb18
Karthik <karthikk1162@gmail.com>
2026-05-01 22:36:49 +0530

    Updated the system prompt to take the csv file if mentioned and to read it using pandas

--- llm.py
+++ llm.py
@@ -2,25 +2,31 @@ import os
 from groq import Groq
 from dotenv import load_dotenv
 
-##Load env variables
 load_dotenv()
 
 client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
 
-def get_llm_response(prompt: str) -> str:
+def get_llm_response(prompt: str, file_path: str = None) -> str:
+
+    system_prompt = """
+You are a Python code generator.
+
+Rules:
+- Only generate Python code
+- If a CSV file is mentioned, use pandas to read it
+- Always print output
+"""
+
+    if file_path:
+        prompt = f"{prompt}\nThe file path is: {file_path}"
+
     try:
         response = client.chat.completions.create(
             model="llama-3.1-8b-instant",
             messages=[
-                {
-                    "role": "system",
-                    "content": "You are a helpful assistant that writes Python code."
-                },
-                {
-                    "role": "user",
-                    "content": prompt
-                }
+                {"role": "system", "content": system_prompt},
+                {"role": "user", "content": prompt}
             ],
             temperature=0.2
         )
